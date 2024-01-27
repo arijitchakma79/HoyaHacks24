941611410a447fc7e4a467528f424e737675d4f7
Arijit Chakma <ac4393@drexel.edu>
2024-01-27 00:43:36 -0500

    set up the client-side app

---
+++ client/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Text, View, ScrollView} from 'react-native'
+
+
+const App = ():JSX.Element => {
+    return (
+        <View>
+            <Text>Hello World</Text>
+        </View>
+    )
+}
+
+export default App;
